fd393a2960b3afc20cc9749f493fd543999c5f36
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-05-22 17:30:22 -0400

    Changed button style

--- pages/404.js
+++ pages/404.js
@@ -4,11 +4,11 @@ import { Col, Container, Row } from "react-bootstrap";
 import ImageComponent from "./../src/components/ImageComponent/ImageComponent";
 import errorImage from "../src/assets/images/error-image.png";
 import styles from "./errorStyle.module.scss";
-import LinearButtonComponent from './../src/components/LinearButtonComponent/LinearButtonComponent';
+import ButtonComponent from '../src/components/ButtonComponent/ButtonComponent';
 
 const errorPage = () => {
     const router = useRouter();
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const onClick = () => {
         setLoading(true);
@@ -29,7 +29,7 @@ const errorPage = () => {
                 <p className={styles.description}>
                   Oops! The page you are looking for does not exist.
                 </p>
-                <LinearButtonComponent title={"Go to Home"} onClick={()=> onClick()} isLoading={loading}/>
+                <ButtonComponent title={"Go to Home"} onClick={()=> onClick()} isLoading={loading}/>
               </div>
             </Col>
 
