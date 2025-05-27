014ea2f9d8be70c708da910770fa759c40c63e79
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-05-27 00:08:14 -0400

    completed forgot api integration

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,8 @@ const Base_URL = "http://localhost:8000/api";
 
 export const AuthAPIRegister = AUTH_URL + "/register"; 
 export const AuthAPILogin = AUTH_URL + "/login"; 
-export const AuthAPIUpdateProfile = AUTH_URL + "/update-profile";
-export const AuthAPIChangePassword = AUTH_URL + "/change-password";
+export const AuthAPIForgotPassword = AUTH_URL + "/forgot-password";
+export const AuthAPIVerifyOtp = AUTH_URL + "/verify-otp";
+export const AuthAPIResetPassword = AUTH_URL + "/reset-password";
 
 
--- src/pages/forgot-password/index.jsx
+++ src/pages/forgot-password/index.jsx
@@ -1,26 +1,49 @@
-
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { apiPost } from "../../utils/utils";
+import { AuthAPIForgotPassword } from "../../api/api";
 import { showError, showSuccess } from "../../utils/helperFunction";
+import Loader from "../../components/Loader";
 
 const ForgotPasswordPage = () => {
   const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const validateEmail = (email) =>
-    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!validateEmail(email)) {
-      showError("Please enter a valid email address.");
-      return;
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  if (!validateEmail(email)) {
+    showError("Please enter a valid email address.");
+    return;
+  }
+
+  try {
+    setLoading(true);
+    const response = await apiPost(AuthAPIForgotPassword, { email });
+
+    if (response?.status === 404) {
+      showError("Email not found");
+    } else if (response?.status === 200) {
+      showSuccess("OTP sent to your email.");
+      navigate("/enter-otp", { state: { email } });
+    } else {
+      showError(response?.message || "An error occurred. Please try again.");
     }
-    showSuccess("OTP sent to your email.");
-    navigate("/enter-otp", { state: { email } });
-  };
+  } catch (error) {
+    console.log("Error status:", error.message || error);
+    showError(error.message || "An error occurred. Please try again.");
+  } finally {
+    setLoading(false);
+  }
+};
 
-  return (
+
+  return loading ? (
+    <Loader />
+  ) : (
     <div className="relative min-h-screen bg-background-dark text-white px-4 py-16 flex justify-center items-center">
       <div className="absolute top-1/4 left-1/4 w-80 h-80 bg-gradient-to-br from-primary to-neon-purple opacity-20 blur-3xl rounded-full" />
       <div className="absolute bottom-1/4 right-1/4 w-72 h-72 bg-gradient-to-tr from-neon-blue to-primary opacity-20 blur-2xl rounded-full" />
