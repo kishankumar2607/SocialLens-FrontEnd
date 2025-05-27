afcf5e3fa4d62534e9cc655af60da784f82e04c8
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-05-27 00:07:48 -0400

    created loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ThreeCircles } from "react-loader-spinner";
+
+const Loader = () => {
+  //styles for the loader
+  const loaderStyles = {
+    width: "100%",
+    height: "90vh",
+    position: "fixed",
+    left: 0,
+    top: 0,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backdropFilter: "blur(8px)",
+    zIndex: 9,
+  };
+  return (
+    <div style={loaderStyles}>
+      <ThreeCircles
+        visible={true}
+        height="100"
+        width="100"
+        color="#4f46e5"
+        ariaLabel="three-circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
+};
+
+export default Loader;
