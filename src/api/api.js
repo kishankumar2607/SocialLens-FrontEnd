f73fdcdc61f2a4a5ecb4adaedcaebbc6a1dd0f78
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-05-30 22:44:11 -0400

    added production api

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,12 @@
+const AUTH_URL = "https://api.sociallens.kishankumardas.com/auth";
+const Base_URL = "https://api.sociallens.kishankumardas.com/api";
 
-const AUTH_URL = "http://localhost:8000/auth";
-const Base_URL = "http://localhost:8000/api";
+// const AUTH_URL = "http://localhost:8000/auth";
+// const Base_URL = "http://localhost:8000/api";
 
 //Auth Api
-export const AuthAPIRegister = AUTH_URL + "/register"; 
-export const AuthAPILogin = AUTH_URL + "/login"; 
+export const AuthAPIRegister = AUTH_URL + "/register";
+export const AuthAPILogin = AUTH_URL + "/login";
 export const AuthAPIForgotPassword = AUTH_URL + "/forgot-password";
 export const AuthAPIVerifyOtp = AUTH_URL + "/verify-otp";
 export const AuthAPIResetPassword = AUTH_URL + "/reset-password";
