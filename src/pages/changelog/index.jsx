a02ea4eef3ae8d30a4a38b5d71196dfeb028c047
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-06-21 22:29:51 -0400

    Update index.jsx

--- src/pages/changelog/index.jsx
+++ src/pages/changelog/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiCheckCircle, FiAlertCircle, FiStar } from "react-icons/fi";
+import { FiCheckCircle, FiStar } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 const changelog = [
