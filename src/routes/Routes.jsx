13a336ef5ede4a93d0ee0be291e550b093f618e9
HarshilSoni7593 <harshilsd1952@gmail.com>
2025-07-29 11:21:59 -0400

    Add Privacy Policy & Terms pages

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -35,248 +35,264 @@ import AnalyticsPage from "../pages/analytics";
 import TryForFree from "../pages/try-for-free";
 
 const Routes = () => {
-  return (
-    <ErrorBoundary>
-      <ScrollToTop />
-      <RouterRoutes>
-        {/* MainLayour Routes */}
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <LandingPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <MainLayout>
-              <LoginPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <MainLayout>
-              <RegisterPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/forgot-password"
-          element={
-            <MainLayout>
-              <ForgotPasswordPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/enter-otp"
-          element={
-            <MainLayout>
-              <EnterOtpPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/reset-password"
-          element={
-            <MainLayout>
-              <ResetPasswordPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/try-for-free"
-          element={
-            <MainLayout>
-              <TryForFree />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/features"
-          element={
-            <MainLayout>
-              <FeaturesPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/pricing"
-          element={
-            <MainLayout>
-              <PricingPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/integrations"
-          element={
-            <MainLayout>
-              <IntegrationsPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/changelog"
-          element={
-            <MainLayout>
-              <ChangelogPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/blogs"
-          element={
-            <MainLayout>
-              <BlogListPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/blogs/:title"
-          element={
-            <MainLayout>
-              <BlogDetailPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/documentation"
-          element={
-            <MainLayout>
-              <DocumentationPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/api-reference"
-          element={
-            <MainLayout>
-              <ApiReferencePage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/guides"
-          element={
-            <MainLayout>
-              <GuidesPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/support"
-          element={
-            <MainLayout>
-              <SupportPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <MainLayout>
-              <AboutPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/careers"
-          element={
-            <MainLayout>
-              <Careers />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <MainLayout>
-              <ContactPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/privacy-policy"
-          element={
-            <MainLayout>
-              <PrivacyPolicy />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/terms-of-service"
-          element={
-            <MainLayout>
-              <TermsOfService />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/cookies"
-          element={
-            <MainLayout>
-              <Cookie />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <MainLayout>
-              <NotFound />
-            </MainLayout>
-          }
-        />
+	return (
+		<ErrorBoundary>
+			<ScrollToTop />
+			<RouterRoutes>
+				{/* MainLayour Routes */}
+				<Route
+					path="/"
+					element={
+						<MainLayout>
+							<LandingPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/login"
+					element={
+						<MainLayout>
+							<LoginPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/register"
+					element={
+						<MainLayout>
+							<RegisterPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/forgot-password"
+					element={
+						<MainLayout>
+							<ForgotPasswordPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/enter-otp"
+					element={
+						<MainLayout>
+							<EnterOtpPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/reset-password"
+					element={
+						<MainLayout>
+							<ResetPasswordPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/try-for-free"
+					element={
+						<MainLayout>
+							<TryForFree />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/features"
+					element={
+						<MainLayout>
+							<FeaturesPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/pricing"
+					element={
+						<MainLayout>
+							<PricingPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/integrations"
+					element={
+						<MainLayout>
+							<IntegrationsPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/changelog"
+					element={
+						<MainLayout>
+							<ChangelogPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/blogs"
+					element={
+						<MainLayout>
+							<BlogListPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/blogs/:title"
+					element={
+						<MainLayout>
+							<BlogDetailPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/documentation"
+					element={
+						<MainLayout>
+							<DocumentationPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/api-reference"
+					element={
+						<MainLayout>
+							<ApiReferencePage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/guides"
+					element={
+						<MainLayout>
+							<GuidesPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/support"
+					element={
+						<MainLayout>
+							<SupportPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/about"
+					element={
+						<MainLayout>
+							<AboutPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/careers"
+					element={
+						<MainLayout>
+							<Careers />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/contact"
+					element={
+						<MainLayout>
+							<ContactPage />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/privacy-policy"
+					element={
+						<MainLayout>
+							<PrivacyPolicy />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/terms-of-service"
+					element={
+						<MainLayout>
+							<TermsOfService />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/cookies"
+					element={
+						<MainLayout>
+							<Cookie />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="*"
+					element={
+						<MainLayout>
+							<NotFound />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/privacy"
+					element={
+						<MainLayout>
+							<PrivacyPolicy />
+						</MainLayout>
+					}
+				/>
+				<Route
+					path="/terms"
+					element={
+						<MainLayout>
+							<TermsOfService />
+						</MainLayout>
+					}
+				/>
 
-        {/* SecondLayout Routes */}
-        <Route
-          path="/homepage"
-          element={
-            <SecondLayout>
-              <Homepage />
-            </SecondLayout>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <SecondLayout>
-              <Dashboard />
-            </SecondLayout>
-          }
-        />
-        <Route
-          path="/create-post"
-          element={
-            <SecondLayout>
-              <CreatePost />
-            </SecondLayout>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <SecondLayout>
-              <SettingsPage />
-            </SecondLayout>
-          }
-        />
-        <Route
-          path="/analytics"
-          element={
-            <SecondLayout>
-              <AnalyticsPage />
-            </SecondLayout>
-          }
-        />
-      </RouterRoutes>
-    </ErrorBoundary>
-  );
+				{/* SecondLayout Routes */}
+				<Route
+					path="/homepage"
+					element={
+						<SecondLayout>
+							<Homepage />
+						</SecondLayout>
+					}
+				/>
+				<Route
+					path="/dashboard"
+					element={
+						<SecondLayout>
+							<Dashboard />
+						</SecondLayout>
+					}
+				/>
+				<Route
+					path="/create-post"
+					element={
+						<SecondLayout>
+							<CreatePost />
+						</SecondLayout>
+					}
+				/>
+				<Route
+					path="/settings"
+					element={
+						<SecondLayout>
+							<SettingsPage />
+						</SecondLayout>
+					}
+				/>
+				<Route
+					path="/analytics"
+					element={
+						<SecondLayout>
+							<AnalyticsPage />
+						</SecondLayout>
+					}
+				/>
+			</RouterRoutes>
+		</ErrorBoundary>
+	);
 };
 
 export default Routes;
